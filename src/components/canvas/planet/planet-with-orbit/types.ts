20917f78942ac524588f6372cb780a204d3be33f
Ethan Konkolowicz <ethan.konkolowicz@gmail.com>
2024-10-06 19:37:27 -0400

    made au values more accurate

--- src/components/canvas/planet/planet-with-orbit/types.ts
+++ src/components/canvas/planet/planet-with-orbit/types.ts
@@ -1,50 +1,51 @@
 export const planetOrbitalData = {
   Mercury: {
-    semiMajorAxis: 0.39,
-    semiMinorAxis: 0.38,
-    eccentricity: 0.2056,
-    inclination: 7.0 * (Math.PI / 180),
+    semiMajorAxis: 0.387098,   // AU
+    semiMinorAxis: 0.379505,   // Calculated from semi-major and eccentricity
+    eccentricity: 0.205630,    // Actual value
+    inclination: 7.00487 * (Math.PI / 180),  // Converted from degrees to radians
   },
   Venus: {
-    semiMajorAxis: 0.72,
-    semiMinorAxis: 0.72,
-    eccentricity: 0.0068,
-    inclination: 3.4 * (Math.PI / 180),
+    semiMajorAxis: 0.723332,   // AU
+    semiMinorAxis: 0.723318,   // Slightly less than the semi-major due to small eccentricity
+    eccentricity: 0.006772,    // Actual value
+    inclination: 3.39471 * (Math.PI / 180),  // Converted from degrees to radians
   },
   Earth: {
-    semiMajorAxis: 1.0,
-    semiMinorAxis: 1.0,
-    eccentricity: 0.0167,
-    inclination: 0.0, 
+    semiMajorAxis: 1.000000,   // AU (defined as 1 AU)
+    semiMinorAxis: 0.999862,   // Calculated value
+    eccentricity: 0.016710,    // Actual value
+    inclination: 0.00005 * (Math.PI / 180),  // Earth's inclination is almost 0
   },
   Mars: {
-    semiMajorAxis: 1.52,
-    semiMinorAxis: 1.51,
-    eccentricity: 0.0934,
-    inclination: 1.85 * (Math.PI / 180),
+    semiMajorAxis: 1.523679,   // AU
+    semiMinorAxis: 1.517504,   // Calculated value
+    eccentricity: 0.093400,    // Actual value
+    inclination: 1.85061 * (Math.PI / 180),  // Converted from degrees to radians
   },
   Jupiter: {
-    semiMajorAxis: 5.20,
-    semiMinorAxis: 5.19,
-    eccentricity: 0.0489,
-    inclination: 1.3 * (Math.PI / 180),
+    semiMajorAxis: 5.202603,   // AU
+    semiMinorAxis: 5.193828,   // Calculated value
+    eccentricity: 0.048900,    // Actual value
+    inclination: 1.30530 * (Math.PI / 180),  // Converted from degrees to radians
   },
   Saturn: {
-    semiMajorAxis: 9.58,
-    semiMinorAxis: 9.56,
-    eccentricity: 0.0565,
-    inclination: 2.5 * (Math.PI / 180),
+    semiMajorAxis: 9.537070,   // AU
+    semiMinorAxis: 9.517243,   // Calculated value
+    eccentricity: 0.056500,    // Actual value
+    inclination: 2.48446 * (Math.PI / 180),  // Converted from degrees to radians
   },
   Uranus: {
-    semiMajorAxis: 19.22,
-    semiMinorAxis: 19.17,
-    eccentricity: 0.0463,
-    inclination: 0.77 * (Math.PI / 180),
+    semiMajorAxis: 19.191263,  // AU
+    semiMinorAxis: 19.159073,  // Calculated value
+    eccentricity: 0.046381,    // Actual value
+    inclination: 0.76986 * (Math.PI / 180),  // Converted from degrees to radians
   },
   Neptune: {
-    semiMajorAxis: 30.05,
-    semiMinorAxis: 30.05,
-    eccentricity: 0.0095,
-    inclination: 1.77 * (Math.PI / 180),
+    semiMajorAxis: 30.068963,  // AU
+    semiMinorAxis: 30.068654,  // Calculated value
+    eccentricity: 0.008678,    // Actual value
+    inclination: 1.76917 * (Math.PI / 180),  // Converted from degrees to radians
   },
 };
+
